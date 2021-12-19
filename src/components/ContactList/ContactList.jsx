1a27508f5a7dc2cefa67aaa1eeb0e6bb38821471
vladislav-slobodenjuk <vladjun@gmail.com>
2021-12-19 14:25:56 +0200

    add propTypes onDelete

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -26,6 +26,7 @@ export default function ContactList(props) {
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object),
+  onDelete: PropTypes.func,
 };
 
 // ContactList.propTypes = {
